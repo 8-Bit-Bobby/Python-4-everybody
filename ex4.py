ce1439c4ad625869d4065b78ef63a0ed2e5c3b81
8-Bit-Bobby <97540310+8-Bit-Bobby@users.noreply.github.com>
2022-08-24 18:18:36 -0700

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,6 @@
+# Write a progrram which prompts the user for a celius temperature, convert the temperature to farhenheit, and print out
+# the converted temperature
+
+celcius = float(input('Enter celcius temperature: '))
+fahrenheit = celcius * 1.8 + 32
+print(fahrenheit)
